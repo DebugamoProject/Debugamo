c3f660f2004a4a4a0015a824da8e48a1334a7ffc
AngusHo <angus88410@gmail.com>
2018-10-17 09:13:00 +0800

    finish leftSide Draft/index.js

--- draft/index/index.js
+++ draft/index/index.js
@@ -38,9 +38,9 @@ $('#login').click(function (e) {
 
 console.log('hello')
 
-/*var lestSide = `
+var leftSide = `
 <div id="title" style="margin:0px auto;">
 <a class="nav-link" href="">Debugamo : 幫幫迪摩</a>
 </div>`
 
-$('.leftSide').append(leftSide);*/
+$('.leftSide').append(leftSide);
