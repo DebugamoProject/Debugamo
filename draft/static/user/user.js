49cc5be7de97b7589ebe1536c48ceb993751e076
Yu-Chun1214 <yuchunlin0075@gmail.com>
2018-11-19 23:14:35 +0800

    commit

--- draft/static/user/user.js
+++ draft/static/user/user.js
@@ -89,13 +89,6 @@ function setLanguage(){
 
 //---------EDIT------------------//
 
-$('#information').on('click',function(e){
-  console.log('edit')
-  $('#main-container').append(edit);
-  $('body').localize();
-  ReloadUserData();
-})
-
 var year,month;
 
 function generateYear(){
@@ -155,15 +148,15 @@ function Init(){
 }
 Init();
 
-$('.fas.fa-pen').on('hover',function(e){
+$('.fas.fa-pen').hover(function(e){
   $(this).css('color','black');
 });
 
-$('.fas.fa-pen').on('mouseout',function(e){
+$('.fas.fa-pen').mouseout(function(e){
   $(this).css('color','grey')
 })
 
-$('.edit').on('click',function(e){
+$('.edit').click(function(e){
   console.log('click');
   $(this).parent().parent().children('.edit-dropdown').slideToggle();
 })
@@ -189,10 +182,8 @@ function ReloadUserData() {
   SetUserData();
 }
 
-
-
 //-----button-click-event-listening------///
-$('#name-button').on('click',function(e) {
+$('#name-button').click(function(e) {
   var data = {
     "name": $('#edit-name').val()
   }
@@ -201,7 +192,7 @@ $('#name-button').on('click',function(e) {
   $('#Name-drop-down').slideToggle();
 })
 
-$('#id-button').on('click',function(e){
+$('#id-button').click(function(e){
   if(!$('#id-notice').length){
     var data = {
       "gameID" : $('#edit-gameID').val()
@@ -212,7 +203,7 @@ $('#id-button').on('click',function(e){
   }
 })
 
-$('#email-button').on('click',function (e) {  
+$('#email-button').click(function (e) {  
   if(!$('#email-notice').length){
     var data = {
       "email" : $('#edit-email-input').val()
@@ -224,7 +215,7 @@ $('#email-button').on('click',function (e) {
   }
 })
 
-$('#password-button').on('click',function (e){
+$('#password-button').click(function (e){
   if(!$('#confirm-notice').length){
     RepeatCheck({
       "password":$('#old-password').val()
@@ -239,7 +230,7 @@ $('#password-button').on('click',function (e){
   }
 })
 
-$('#birthday-button').on('click',function (e) {
+$('#birthday-button').click(function (e) {
   var data =  $('#year').val()+'-'+$('#month').val()+'-'+$('#date').val()
   update_data({
     "birthday":data
@@ -248,7 +239,7 @@ $('#birthday-button').on('click',function (e) {
   $('#Birthday-drop-down').slideToggle();
 })
 
-$('#class-button').on('click',function (e){
+$('#class-button').click(function (e){
   update_data({
     "class":$('#class-input').val()
   },'class')
@@ -285,7 +276,7 @@ function IsAvailable(notice_id){
   $('#'+notice_id).remove();
 }
 
-$('#edit-gameID').on('change',function(e){
+$('#edit-gameID').change(function(e){
   // var text = $('#edit-gameID').val();
   var text = this.value;
   console.log(text);
@@ -294,7 +285,7 @@ $('#edit-gameID').on('change',function(e){
   },'#id-input','This ID has been registered','id-notice','False')
 })
 
-$('#edit-email-input').on('change',function(e){
+$('#edit-email-input').change(function(e){
   // var text = $('#edit-email-input').val();
   var text = this.value;
   console.log(text);
@@ -304,19 +295,19 @@ $('#edit-email-input').on('change',function(e){
   },'#email-input-div','This email has been registered','email-notice','False')
 })
 
-$('#old-password').on('change',function (e){
+$('#old-password').change(function (e){
   $('#old-password-notice').remove();
 })
 
 //--------two passwords are same?----//
 
-$('#newpassword').on('change',function(e){
+$('#newpassword').change(function(e){
   if(this.value == $('#confirm').val()){
     IsAvailable('confirm-notice')
   }
 })
 
-$('#confirm').on('change',function (e){
+$('#confirm').change(function (e){
   var text = this.value;
   console.log(text)
   if(text != $('#newpassword').val()){
@@ -346,203 +337,3 @@ $('select#language-select').on('change',function(e){
   console.log('cookies set again')
   window.location.reload(true);
 })
-
-let edit = `
-<style>
-
-/*-------edit part----------*/
-
-#edit{
-    /* display: grid; */
-    width: 90%;
-    padding: 5%;
-    padding-top: 5%;
-    box-sizing: border-box;
-    /* grid-template-rows: 80px 80px 80px 80px 80px; */
-}
-
-.item{
-    width: 100%;
-    padding: 10px;
-    
-}
-
-#edit label{
-    font-size: 16px;
-} 
-
-.item input{
-    outline: none;
-    border:none;
-    border-bottom: 1px solid black;
-    border-radius: 5px 5px 0px 0px;
-    width: 20%;
-    height: 30px;
-    font-size: 18px;
-    vertical-align: bottom;
-    margin-right:10px;
-    background-color:transparent;
-}
-
-.item input:focus{
-    transition: 1s;
-    background-color: rgb(255, 222, 180);
-}
-
-#name-ID{
-    display: grid;
-    grid-template-columns: 50% 50%;
-}
-
-#edit span{
-    margin-right:10px;
-}
-
-#name-ID input{
-    width: 50%;
-}
-
-#edit div.edit-dropdown{
-    margin-top: 10px;
-    background-color: antiquewhite;
-    box-shadow:3px 3px 9px grey;
-    /* height: 50px; */
-    display: none;
-    padding:10px;
-    padding-left: 0px;
-    height: 5%;
-}
-
-
-button{
-    outline: none;
-    height: 30px;
-    font-size:14px;
-    border-radius: 3px;
-}
-
-button:hover{
-    transition: 0.5s;
-    background-color: rgb(232, 226, 226);
-}
-
-.user-data{
-    margin-right:10px;
-}
-
-
-#name-ID .edit-dropdown{
-    width: 80%;
-}
-
-div#password-dropdown.edit-dropdown{
-    font-size: 12px;
-    height: 80px;
-}
-#password-dropdown.edit-dropdown input{
-    width:18%
-}
-
-#Birthday-drop-down select{
-    width: 60px;
-    height: 30px;
-    font-size: 14px;
-}
-
-</style>
-<div id="edit">
-<div id="edit-title">
-  <span style="font-size: 40px" data-i18n="[append]edit.title.edit">Edit</span>
-  <div style="border: 1px solid lightgrey"></div>
-</div>
-<div id="name-ID" class="item">
-  <div id="name"> 
-    <div style="display: inline">
-      <label for="user-name" data-i18n="[append]edit.nameid.name.name">Name : </label>
-
-      <i class="fas fa-pen edit" id="name-edit" style="color:grey"></i>
-    </div>
-    <div id="Name-drop-down" class="edit-dropdown">
-      <label for="edit-name" data-i18n="[append]edit.nameid.name.name">Name : </label>
-      <input type="text" id="edit-name"> <button id="name-button" data-i18n="[append]edit.button">submit</button>
-      
-    </div>
-  </div>
-  <div id="ID">
-    <div>
-      <label for="edit-id" data-i18n="[append]edit.nameid.id.gameid">gameID : </label>
-      
-      <i class="fas fa-pen edit" id="ID-edit" style="color:grey"></i>
-    </div>
-    <div id="ID-drop-down" class="edit-dropdown">
-      <div style="width:100%" id="id-input">
-        <label for="edit-id" data-i18n="[append]edit.nameid.id.gameid">gameID : </label>
-        <input type="text" id="edit-gameID"> <button id="id-button" data-i18n="[append]edit.button">submit</button>
-      </div>
-      <!-- <small style="display:inline">adslfk;askljf;alskjf</small> -->
-    </div>
-  </div>
-</div>
-<div id="edit-email" class="item">
-  <div>
-    <label for="edit-email" data-i18n="[append]edit.email.email">Email : </label>
-    <!-- <span id="user-email" class="user-data">example@gmail.com</span> -->
-    <i class="fas fa-pen edit" id="email-edit"style="color:grey"></i>
-  </div>
-  <div id="Email-drop-down" class="edit-dropdown" style="width:50%;">
-    <div id="email-input-div" style="width:100%;">
-      <label for="edit-email" data-i18n="[append]edit.email.email">Email : </label>
-      <input type="email" style="width:60%;" id="edit-email-input"> <button id="email-button" data-i18n="[append]edit.button">Submit</button>
-    </div>
-  </div>
-</div>
-<div id="edit-password" class="item">
-  <div>
-    <label for="password" data-i18n="[append]edit.password.password">Password :</label>
-    <span id="password">********</span>
-    <i class="fas fa-pen edit" style="color:grey"></i>
-  </div>
-  <div id="password-dropdown" class="edit-dropdown" style="display:none;width:90%;">
-    <div >
-      <label for="old-password" data-i18n="[append]edit.password.old-password">Old Password</label>
-      <input type="password" style="display:inline;" id="old-password">
-    </div>
-    <div id="password-confirm">
-      <label for="new-password" data-i18n="[append]edit.password.new-password">New Password</label>
-      <input type="password" style="margin-top:5px;" id='newpassword'>
-      <label for="comfirm" data-i18n="[append]edit.password.confirm">Confirm</label>
-      <input type="password" id='confirm'>
-      <button id="password-button" data-i18n="[append]edit.button">submit</button>
-    </div>
-  </div>
-</div>
-<div id="edit-birthday" class="item">
-  <div>
-    <label for="user-birthday" data-i18n="[append]edit.birthday">Birthday : </label>
-    <i class="fas fa-pen edit" id="birthday-edit"style="color:grey"></i>
-  </div>
-  <div id="Birthday-drop-down" class="edit-dropdown" style="width:50%;">
-    <label for="edit-birthday">Birthday : </label>
-    <select name="year" id="year" class="form-cotrol"></select>
-    <select name="month" id="month"></select>
-    <select name="date" id="date"></select>
-    <button style="margin-left:20px;" id="birthday-button" data-i18n="[append]edit.button">Submit</button>
-  </div>
-</div>
-<div id="edit-class" class="item">
-  <div>
-    <label for="edit-class" data-i18n="[append]edit.class">Class : </label>
-    
-    <i class="fas fa-pen edit" id="class-edit"style="color:grey"></i>
-  </div>
-  <div id="class-dropdown" class="edit-dropdown" style="width:30%;">
-    <label for="edit-class" data-i18n="[append]edit.class">Class : </label>
-    <input type="text" style="width:38%" id="class-input">
-    <button id="class-button" data-i18n="[append]edit.button">Submit</button>
-  </div>
-</div>
-</div>
-<script>
-
-</script>
-`
