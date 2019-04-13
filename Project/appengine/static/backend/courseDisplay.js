218bd3b793cfb591f962c1707308d27a68f6a4e0
AngusHo <angus2313@gmail.com>
2019-04-14 00:00:10 +0800

    teacher page frontend basic finished

--- Project/appengine/static/backend/courseDisplay.js
+++ Project/appengine/static/backend/courseDisplay.js
@@ -6,6 +6,7 @@ $('#course-statistic').on('click', function(e) {
         let student_list_html = $(".student-list").parent()[0].outerHTML;
         let task_list_html = $(".task-list").parent()[0].outerHTML;
         course_setting_html = student_list_html + task_list_html;
+        // console.log(course_setting_html);
         $(".student-list").parent().remove();
         $(".task-list").parent().remove();
 
@@ -13,20 +14,35 @@ $('#course-statistic').on('click', function(e) {
         
     }
     catch {
-        $("#statistic-chart").remove();
+        $("#statistic-selected").remove();
         console.log('reclear');
     }
-    
-    // statistic
-    let chart = '<div class="col-9" id="statistic-chart" style="min-width: 310px; height: 400px; margin: 0 auto"></div>';
+
+    let course_id = $('.list-group-item-primary')[0].id;
+    console.log(course_id);
+
+    let chart = `
+        <div class="col-9" id="statistic-selected">
+            <div class="row">
+                <div class="card" style="margin: 20px">
+                    <nav class="nav nav-tabs nav-fill">
+                        <li class="nav-item nav-link chart-selector active" data-toggle="tab" id="pass-num-statistic">通過人數統計</li>
+                        <li class="nav-item nav-link chart-selector" data-toggle="tab" id="pass-time-statistic">通過時間統計</li>
+                        <li class="nav-item nav-link chart-selector" data-toggle="tab" id="student-statistic">學生過關統計</li>
+                    </nav>
+                </div>
+            </div>
+            <div id="statistic-chart" style="min-width: 310px; height: 400px; margin: 10px;"></div>
+        </div>`;
+
     $("#side-selector .row").first().append(chart);
-    makeChart();
+    makeChart('pass-num-statistic', course_id);
 })
 
 $('#course-setting').on('click', function(e) {
-    console.log(course_setting_html);
+    // console.log(course_setting_html);
     $(".student-list").parent().remove();
     $(".task-list").parent().remove();
-    $("#statistic-chart").remove();
+    $("#statistic-selected").remove();
     $("#side-selector .row").first().append(course_setting_html);
 })
--- Project/appengine/static/backend/coursesManager1.js
+++ Project/appengine/static/backend/coursesManager1.js
@@ -4,12 +4,17 @@ function getCourse(){
     var courses = courseProcess(url + '/courses');
     courses = coursesSetup(courses);
     var coursesBar = new Array;
+    let is_first = true;
     for(var c of courses){
         var classItem = document.createElement('li')
         classItem.className = 'classItem list-group-item courseManage memberManage';
+        if(is_first) {
+            classItem.className = classItem.className + ' list-group-item-primary';
+            is_first = false;
+        }
         classItem.id = c.id;
-        var a = document.createElement('a');
-        a.href = 'javascript:void(0)';
+        classItem.style = 'cursor: pointer';
+        var a = document.createElement('nav');
         // a.id = c.id;
         a.className = 'course';
         a.innerText = c.id;
@@ -51,21 +56,40 @@ $('.course').on('click',function(e){
 })
 
 $(document).on('click', '.courseManage', function(){
+    // remove all left pannel highlight
+    $(".courseManage").removeClass('list-group-item-primary');
+
+    if($('.task-list').length === 0) {
+        let course_setting = `
+            <div class="col-4" style="margin-top:20px">
+                <div class="card-header text-center">學生列表</div>
+                <ul class="list-group student-list course-data text-center"></ul>
+            </div>
+            <div class="col-4" style="margin-top: 20px">
+                <div class="card-header text-center">關卡列表</div>
+                <div class="text-center task-list course-data"></div>
+            </div>`;
+        $("#statistic-selected").remove();
+        $("#side-selector .row").first().append(course_setting_html);
+    }
     console.log('in course');
     console.log(courses);
     $('.task-list').children('div').remove();
     $('.task-list').append(courses[$(this)[0].id]);
     // $(this).parent().slideToggle();
+
+    // highlight the left pannel course that is selected
+    $('#courses #' + $(this)[0].id).addClass('list-group-item-primary');
 })
 
 
 $(document).on('click', '.memberManage', function(e){
+    // $("#statistic-selected").remove();
     // $(this).parent().slideToggle();
     $('.student-list').empty();
     // setting current course attribute
     var id = $(this)[0].id;
     console.log(id);
-    $('.current_course').attr('current_course', id);
     var members =  getMembers(id);
     console.log(members);
     // add members to list
@@ -114,3 +138,25 @@ function getInputValue() {
     var nameValue = document.forms["input_form"]["input_member"].value;
     return nameValue;
 }
+
+
+function initContent() {
+    // class id for student
+    $('#course_id_display').append('afefwe');
+
+    let id = $('.memberManage.list-group-item-primary')[0].id
+    // task-list
+    $('.task-list').append(courses[id]);
+    
+    
+    // student-list
+    var members =  getMembers(id);
+    // add members to list
+    let item = '';
+    for(var i = 0; i < members.length; i++) {
+        item = '<li class="member_item list-group-item">' + members[i] + '</li>';
+        $('.student-list').append(item);
+    }
+}
+
+initContent();
